f06f549bea9da9d391379dd2f31c98b1a9830efa
astephens-fsd <astephens.pm@icloud.com>
2021-09-13 14:24:02 -0700

    Task 1 completed

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter 1 has a closure, counter 2 does nt have a closure
+
   2. Which of the two uses a closure? How can you tell?
+  Counter 1, since there is a return function within counterMaker function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
